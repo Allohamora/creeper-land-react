5abc905ec0fc3dd5012b9d1a430f485a20d1dadd
Allohamora <herman2000@yandex.ru>
2020-12-23 13:13:43 +0200

    Override refactor

--- src/components/Override/Override.tsx
+++ src/components/Override/Override.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BaseProps, DivProps } from 'types/props';
 
-export interface OverrideProps extends DivProps, BaseProps {
+export interface OverrideProps extends BaseProps, DivProps {
   component?: React.ElementType<DivProps>;
+  [prop: string]: any;
 }
 
 const Override: React.FC<OverrideProps> = ({
