6a629e970851eb75f21d50e5f3c9c526be014d63
Allohamora <herman2000@yandex.ru>
2021-07-10 12:26:57 +0300

    feat: add rubik font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
 import theme from 'styles/theme';
 import GlobalStyles from 'styles/GlobalStyles';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyles />
+  <>
+    <Head>
+      <link href="/fonts/rubik/rubik.css" rel="stylesheet" />
+    </Head>
 
-    <Component {...pageProps} />
-  </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
 );
 
 export default App;
